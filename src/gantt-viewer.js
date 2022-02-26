a5d64cfc6e6c3ba6920dc3be5b4da408f23e9dc8
yo16 <xxyo1xx@gmail.com>
2022-02-27 00:50:56 +0900

    背景のスプライトを描画

--- src/gantt-viewer.js
+++ src/gantt-viewer.js
@@ -9,10 +9,10 @@ class GanttViewer{
     }
 
     // データをもとに描画
-    data(cur_data){
+    data(original_data){
         // 日付のmin/maxを取得
-        let min_dt = d3.min(cur_data.map(d => new Date(d['start_dt'])));
-        let max_dt = d3.max(cur_data.map(d => new Date(d['end_dt'])));
+        let min_dt = d3.min(original_data.map(d => new Date(d['start_dt'])));
+        let max_dt = d3.max(original_data.map(d => new Date(d['end_dt'])));
         let graph_min_dt = min_dt;
         let graph_max_dt = max_dt;
         graph_min_dt.setDate(min_dt.getDate() - min_dt.getDay() - 6);   // 前の週の月曜日を取得
@@ -27,10 +27,10 @@ class GanttViewer{
         //       ...
         // の形にする
         let data = [];
-        for(let i=0; i<cur_data.length; i++){
+        for(let i=0; i<original_data.length; i++){
             let line_data = [];
-            let start_dt = new Date(cur_data[i]['start_dt']);
-            let end_dt = new Date(cur_data[i]['end_dt']);
+            let start_dt = new Date(original_data[i]['start_dt']);
+            let end_dt = new Date(original_data[i]['end_dt']);
             for(
                 let dt=start_dt;
                 dt<=end_dt;
@@ -47,13 +47,13 @@ class GanttViewer{
         const padding_right = 10;
         const padding_top = 20;
         const padding_bottom = 10;
-        const one_line_height = 40;
-        const one_line_width = 30;
+        const one_line_height = 60;     // １行の高さ
+        const one_line_width = 30;      // 線の太さ
 
         // 全体設定
         let dom = d3.select(this.dom_id);
         let svg_width = dom.node().clientWidth;
-        let svg_height = one_line_height * cur_data.length + padding_top + padding_bottom;
+        let svg_height = one_line_height * original_data.length + padding_top + padding_bottom;
         let svg = dom
             .append("svg")
             .attr("width", svg_width)
@@ -66,14 +66,43 @@ class GanttViewer{
             .range([padding_left, svg_width-padding_right]);
         const yScale = d3
             .scaleLinear()
-            .domain([0, cur_data.length+1])
+            .domain([0.5, original_data.length+0.5])
             .range([padding_top, svg_height-padding_bottom]);
+
+        // 背景の縞模様を描画
+        this.draw_background(svg, original_data, yScale, one_line_height);
 
         // 軸を描画
         this.draw_axis(svg, xScale, yScale, week_num, padding_top, padding_left);
         
         // 線を描画
-        this.draw_lines(svg, data, cur_data, xScale, yScale, one_line_width);
+        this.draw_lines(svg, data, original_data, xScale, yScale, one_line_width);
+
+        // 説明を描画
+        this.draw_items(svg, original_data, yScale, one_line_height);
+    }
+
+
+    // 背景の縞模様を描画
+    draw_background(svg, original_data, yScale, one_line_height){
+        let cur_data = [];
+        // 1行おきに間引く
+        for(let i=1; i<original_data.length; i+=2){
+            cur_data.push(i+1);
+        }
+
+        // 描画
+        svg
+            .selectAll("rect.background-stripe")
+            .data(cur_data)
+            .enter()
+            .append("rect")
+            .attr("class", "background-stripe")
+            .attr("x", "0")
+            .attr("y", function(d){ return yScale(d)-(one_line_height/2);})
+            .attr("width", svg.node().clientWidth)
+            .attr("height", one_line_height)
+            .attr("fill", "#f3f3f3");
     }
 
     
@@ -92,7 +121,7 @@ class GanttViewer{
         // y軸を描画
         const axisy = d3
             .axisLeft(yScale)
-            .ticks(6);    // ★最終的には0にする
+            .ticks(5);
         let y = svg
             .append("g")
             .attr("class", "axis axis-y")
@@ -135,7 +164,7 @@ class GanttViewer{
                 .attr("class", "line-point"+(i+1))
                 .attr("cx", function(d){ return xScale(d[0]); })
                 .attr("cy", function(d){ return yScale(d[1]); })
-                .attr("fill", "rgba(255,255,255,0.6)")
+                .attr("fill", "rgba(255,255,255,0.4)")
                 .attr("r", 4);
         }
         
@@ -160,7 +189,8 @@ class GanttViewer{
             .data(min_dts)
             .enter()
             .append("text")
-            .attr("class", "line-point-start")
+            .attr("class", "line-point-dt")
+            .attr("class", "line-point-start-dt")
             .attr("x", function(d){ return xScale(d[0])-19; })
             .attr("y", function(d){ return yScale(d[1])+7; })
             .attr("text-anchor", "end")
@@ -171,10 +201,32 @@ class GanttViewer{
             .data(max_dts)
             .enter()
             .append("text")
-            .attr("class", "line-point-start")
+            .attr("class", "line-point-dt")
+            .attr("class", "line-point-end-dt")
             .attr("x", function(d){ return xScale(d[0])+19; })
             .attr("y", function(d){ return yScale(d[1])+7; })
             .text(function(d){ return dt2str(d[0]) })
             .attr("fill", "rgba(0,0,0,0.8)");
+    }
+
+
+    // 説明書きを描画
+    draw_items(svg, original_data, yScale, one_line_height){
+    /*
+        const padding_left = 5;
+
+        // 
+        svg
+            .selectAll("rect.item")
+            .data(original_data)
+            .enter()
+            .append("rect")
+            .attr("class", "item")
+            .attr("x", function(d, i){ return 0; })
+            .attr("y", function(d, i){ return yScale(i+1)-(one_line_height/2); })
+            .attr("width", svg.node().clientWidth)
+            .attr("height", one_line_height)
+            .attr("fill", "#f00");
+    */
     }
 }
